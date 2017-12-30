a74b1026c8ea412cc579a8fd5d82b804775296d2
astrosquid <varcap.cb@gmail.com>
2017-12-30 01:30:08 -0500

    Adding more header functionality. Beginning checks.

--- converter.py
+++ converter.py
@@ -34,10 +34,19 @@ class Tag:
     def wrap_tag(self):
         return '<{}>{}</{}>' % {self.html_tag, self.content, self.html_tag}
 
+    def add_content(self, content):
+        self.content = content
+
 class Header(Tag):
-    def __init__(self, level):
+    def __init__(self):
         super().__init__()
-        self.level = level
+        self.level = 0
+
+    def add_level(self):
+        self.level += 1
+
+    def get_level(self):
+        return self.level
 
 class Link(Tag):
     def __init__(self, link):
@@ -48,15 +57,47 @@ class Link(Tag):
     def wrap_tag(self):
         return '<{} href=\'{}\'>{}</{}>' % {self.html_tag, self.content, self.html_tag}
 
-def begin_conversion():
+def make_header(line):
+    header = Header()
+    limit = 6
+    count = 0
+    while count < limit:
+        if line[count] == '#':
+            header.add_level()
+            count += 1
+        else 
+            break
+    
+    header.add_content(line[(count+1):])
+
+    return header
+
+# make a stack for every element we're converting
+def analyze_line(line):
+    specials = ['#', '*', '_', '[', ']', '(', ')']
+    used = [] # stack
+    output = [] # stack
+
+    # if this line is just a newline... 
+
+    # if this line is a header...
+    if line[0] == '\n':
+        output += line
+        return output
+    elif line[0] == '#':
+        header = make_header(line)
+    
+    for char in line:
+        if char in specials:
+            if char == used[len(used - 1)]:
+                # create object 
+                x = 1
+
+def convert():
     path = argv[1]
     file_ops = FileOps(path)
     file_ops.open_files()
-
-    specials = ['#', '*', '_', '[', ']', '(', ')']
     
     stack = []
     for line in file_ops.content:
-        for char in line:
-            if char in specials:
-                
+        analyze_line(line)
