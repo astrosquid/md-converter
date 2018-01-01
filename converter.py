0fa33c37be070a6a0cc41c34d1f470981f658040
astrosquid <varcap.cb@gmail.com>
2017-12-31 20:35:35 -0500

    Narrowing down process, about to make Stack class.

--- converter.py
+++ converter.py
@@ -25,23 +25,43 @@ class FileOps:
 class Tag:
     def __init__(self, html_tag):
         self.html_tag = html_tag
+        self.content = ""
 
     def wrap_tag(self):
+        """Create HTML representation of self."""
         return '<{} class="why-article">{}</{}>' % {self.html_tag, self.content, self.html_tag}
 
     def add_content(self, content):
-        self.content = content
+        """Add string literal content to self."""
+        self.content += content
 
     def get_content(self):
+        """Return string literal content."""
         return self.content
 
     def elim_notation(self):
+        """Permanently eliminate markdown notation; careful when using for Link class."""
         self.content = self.content[1:len(self.content)-1]
 
+    def no_notation(self):
+        """Create and return string literal content without markdown notation."""
+        return self.content[1:len(self.content)-1]
+
 class Header(Tag):
-    def __init__(self):
+    def __init__(self, line):
         super().__init__('h')
+        # TODO: can probably cut out 'count' here
         self.level = 0
+        limit = 6
+        count = 0
+        while count < limit:
+            if line[count] == '#':
+                self.add_level()
+                count += 1
+            else:
+                break
+
+        self.add_content(line[(count+1):])
 
     def add_level(self):
         self.level += 1
@@ -56,7 +76,15 @@ class Link(Tag):
         self.link = link
 
     def wrap_tag(self):
+        # TODO: fix this
         return '<{} href=\'{}\'>{}</{}>' % {self.html_tag, self.content, self.html_tag}
+
+    def elim_notation(self):
+        # TODO
+        # modify this to look for:
+        # the first and last parens before the first open bracket
+        # the first open bracket and last close bracket after the parens
+        pass
 
 class Formatted(Tag):
     def __init__(self):
@@ -68,44 +96,49 @@ class Escape():
     def __init__(self, esc_char):
         self.char = esc_char
 
-def make_header(line):
-    header = Header()
-    limit = 6
-    count = 0
-    while count < limit:
-        if line[count] == '#':
-            header.add_level()
-            count += 1
-        else:
-            break
-    
-    header.add_content(line[(count+1):])
+def decide_tag(char):
+    if char == '*':
+        return Tag('b')
+    elif char == '_':
+        return Tag('i')
+    elif char == '[':
+        return Link()
 
-    return header
-
-# make a stack for every element we're converting
 def analyze_line(line):
+    """Create HTML string literal of the line passed in."""
+    markdown = {
+        '#' : '\n',
+        '*' : '*',
+        '_' : '_',
+        '[' : ']',
+        '(' : ')'
+    }
     specials = ['#', '*', '_', '[', ']', '(', ')']
-    # [Bold, Italics, Link, Preformatted]
-    used = [False, False, False, False]
     output = [] # stack
 
-    # if this line is just a newline... 
+    # If this line is just a newline... 
     if line[0] == '\n':
+        # ...add it to output and return...
         output += line
         return output
-    # if this line is a header...
     elif line[0] == '#':
-        header = make_header(line)
+        # ...or, make a header...
+        header = Header(line)
+        output.append(header)
     else:
-        # make a new paragraph
+        # ...or make a new paragraph.
         output.append(Tag('p'))
 
     for char in line:
         if char in specials:
-            if char == used[len(used - 1)]:
-                # create object 
-                x = 1
+            # is this a new element?
+            if char == output.last:
+                # wrap up this element, append to last element's content
+                pass
+            else:
+                tag = decide_tag(char)
+        else:
+            output.append(char)
 
 def convert():
     path = argv[1]
