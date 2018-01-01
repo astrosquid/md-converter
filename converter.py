73d25d4dde7aa5a0bce107568202846ce2bcb881
astrosquid <varcap.cb@gmail.com>
2018-01-01 14:19:37 -0500

    Whoa it's starting to work.

--- converter.py
+++ converter.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 from sys import argv
+import sys
 
 class Stack:
     """Quick stack implementation using a Python list."""
@@ -10,12 +11,14 @@ class Stack:
 
     def push(self, element):
         """Push an element to the top of the stack."""
+        print("Pushing into the stack: " + element.__class__.__name__)
         self.stack.append(element)
 
     def pop(self):
         """Pop an element off the top of the stack."""
         elem = self.stack[-1]
         del self.stack[-1]
+        print("Popping off the stack: " + elem.__class__.__name__)
         return elem
 
     def peek(self):
@@ -43,6 +46,9 @@ class FileOps:
         self.in_file.close()
         self.out_file.close()
 
+    def get_content(self):
+        return self.content
+
 class Tag():
     def __init__(self, html_tag):
         self.html_tag = html_tag
@@ -53,7 +59,7 @@ class Tag():
     
     def wrap_tag(self):
         """Create HTML representation of self."""
-        return '<{} class="why-article">{}</{}>' % {self.html_tag, self.content, self.html_tag}
+        return '<{} class="why-article">{}</{}>'.format(self.html_tag, self.content, self.html_tag)
 
     def add_content(self, content):
         """Add string literal content to self."""
@@ -74,7 +80,6 @@ class Tag():
 class Header(Tag):
     def __init__(self, line):
         super().__init__('h')
-        # TODO: can probably cut out 'count' here
         self.level = 0
         limit = 6
         while self.level < limit:
@@ -85,10 +90,13 @@ class Header(Tag):
 
     def add_level(self):
         self.level += 1
-        self.html_tag = 'h' + str(self.level)
 
     def get_level(self):
         return self.level
+
+    def wrap_tag(self):
+        self.html_tag = self.html_tag + str(self.level)
+        return super().wrap_tag()
 
 class Link(Tag):
     def __init__(self, link):
@@ -134,7 +142,11 @@ def decide_tag(char):
     elif char == '[':
         return Link("")
 
+    print(char + " is not special, aborting.")
+    sys.exit(1)
+
 def is_ending_tag(char, tag):
+    print('Passed to ending checker: ' + char + tag)
     ending_tags = {
         'p' : '\n',
         'h' : '\n',
@@ -143,7 +155,7 @@ def is_ending_tag(char, tag):
         'a' : ']'
     }
 
-    if char in ending_tags.keys():
+    if tag in ending_tags.keys():
         if ending_tags[tag] == char:
             return True
 
@@ -159,16 +171,19 @@ def analyze_line(line):
         '(' : ')'
     }
 
-    specials = ['#', '*', '_', '[', ']', '(', ')']
+    specials = ['*', '_', '[', ']', '(', ')']
     output = Stack() # stack
 
     if line[0] == '#':
-        # ...or, make a header...
+        # Make a header...
         header = Header(line)
         output.push(header)
+        # Eliminate group of # at beginning of line.
     else:
         # ...or make a new paragraph.
         output.push(Tag('p'))
+
+    block = ""
 
     for char in line:
         if char in specials:
@@ -177,25 +192,37 @@ def analyze_line(line):
                 # wrap up this element, append to last element's content
                 output.peek().add_content(char)
                 elem = output.pop()
-                output.peek().add_content(elem.elim_notation)
-            #
+                output.peek().add_content(elem.wrap_tag)
             else:
-                tag = decide_tag(char)
+                output.push(decide_tag(char))
+        elif char == '\n':
+            block += output.pop().wrap_tag()
         else:
             output.peek().add_content(char)
 
-    return output
+    return block
+
+def list_to_str(l):
+    x = ""
+    for y in l:
+        x += y
+    
+    return x
 
 def convert():
     path = argv[1]
-    file_ops = FileOps(path)
-    file_ops.open_files()
+    out_path = path.split('.')[0] + '.html'
     
     output = []
-    for line in file_ops.content:
-        output.append(analyze_line(line))
 
-    file_ops.write(output)
-    file_ops.close_files()
+    with open(path) as f:
+        for line in f:
+            output.append(analyze_line(line))
+
+    out_file = open(out_path, 'w')
+    for line in output: 
+        out_file.write(str(output))
+    out_file.close()
+    #file_ops.close_files()
 
 convert()
